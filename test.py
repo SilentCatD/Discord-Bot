122abfe4e26a5ae2b1ab94cbf4c9cd1e27e75f03
SilentCatD <36961907+SilentCatD@users.noreply.github.com>
2021-07-23 19:44:04 +0700

    Add weather, covid small api

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import requests
-API_key = 'd715f170763664cefb4ba83e161e58f9'
+api_key = 'd9dd7595f02577a2bf2b9f385ebc1c64'
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
 city_name = 'Ho Chi Minh'
-response = requests.get('https://api.covid19api.com/summary')
+complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+response = requests.get(complete_url)
 print(response.json())
